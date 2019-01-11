8e63000a9653cd050b677b64541fd9287ada93e7
Puncoz Nepal <puncoz138@gmail.com>
2019-01-11 16:28:58 +0545

    styling

--- js/index.js
+++ js/index.js
@@ -2,81 +2,155 @@ var map;
 var markers = [];
 var autocomplete;
 var infoWindow;
-var locationSelect;
+var locationListWrapper;
 var csvData = [];
 var australia = { lat: -25.2744, lng: 133.7751 };
+var importFrom = "sheet";
+
+$(document).on("click", ".locationlistitem", function (event) {
+  var markerNum = $(this).data("index");
+  if (markerNum != "none") {
+    google.maps.event.trigger(markers[markerNum], "click");
+  }
+});
+
+function createLocationList(data) {
+  let template = `
+    <li class="list-marker locationlistitem" data-index="__ID__">
+          <div class="list-details">
+            <div class="list-content">
+              <div class="name">__name__</div>
+              <div class="address">__address__</div>
+              <div class="telephone"><a href="tel:__tel__">__tel__</a></div>
+              <div class="direction">
+                <a href="__link__" target="_blank">Get Direction</a>
+              </div>
+            </div>
+          </div>
+        </li>
+  `;
+
+  let lists = "";
+
+  locationListWrapper.html("");
+  console.log(data);
+  data.forEach((list, index) => {
+    let listHtml = template;
+    listHtml = listHtml.replace(/__ID__/g, index);
+    listHtml = listHtml.replace(/__name__/g, list.name);
+    listHtml = listHtml.replace(/__address__/g, list.address);
+    listHtml = listHtml.replace(/__tel__/g, list.telephone);
+    listHtml = listHtml.replace(
+      /__link__/g,
+      getDirectionUrl(list.address, list.lat, list.lng)
+    );
+
+    lists += listHtml;
+  });
+
+  locationListWrapper.html(lists);
+}
+
+function getDirectionUrl(address, lat, lng) {
+  return `https://www.google.com/maps/dir/Current+Location/${address}/@${lat},${lng},15z`;
+}
+
+function importGoogleSheetData() {
+  initTableTop();
+
+  function initTableTop() {
+    var publicSpreadsheetUrl =
+      "GOOGLE_SHEET_URL";
+
+    Tabletop.init({
+      key: publicSpreadsheetUrl,
+      callback: function (data, tabletop) {
+        csvData = data;
+        searchLocationsNear(null);
+      },
+
+      simpleSheet: true
+    });
+  }
+}
 
 function importCSV() {
   Papa.parse("js/collection-centers.csv", {
     download: true,
     complete: function (results) {
-      csvData = results.data
+      csvData = results.data;
 
-      searchLocationsNear(null)
+      searchLocationsNear(null);
     }
-  })
+  });
 }
 
 function resetMap() {
-  searchLocationsNear(null)
-  document.getElementById('addressInput').value = ''
+  searchLocationsNear(null);
+  document.getElementById("addressInput").value = "";
   var latlng = new google.maps.LatLng(australia.lat, australia.lng);
   map.setCenter(latlng);
   map.setZoom(5);
 }
 
 function initMap() {
-  importCSV()
+  if (importFrom === "sheet") {
+    importGoogleSheetData();
+  } else {
+    importCSV();
+  }
 
-  map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById("map"), {
     center: australia,
     zoom: 5,
-    mapTypeId: 'roadmap',
-    mapTypeControlOptions: { style: google.maps.MapTypeControlStyle.DROPDOWN_MENU }
+    mapTypeId: "roadmap",
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+    }
   });
 
   infoWindow = new google.maps.InfoWindow();
 
-  listenAutocomplete()
+  listenAutocomplete();
 
   radiusSelect = document.getElementById("radiusSelect");
   radiusSelect.onchange = function () {
-    google.maps.event.trigger(autocomplete, 'place_changed');
-  }
+    google.maps.event.trigger(autocomplete, "place_changed");
+  };
 
   document.getElementById("resetButton").onclick = resetMap;
 
-  locationSelect = document.getElementById("locationSelect");
-  locationSelect.onchange = function () {
-    var markerNum = locationSelect.options[locationSelect.selectedIndex].value;
-    if (markerNum != "none") {
-      google.maps.event.trigger(markers[markerNum], 'click');
-    }
-  };
+  locationListWrapper = $("#locationListWrapper");
 }
 
 function listenAutocomplete() {
-  autocomplete = new google.maps.places.Autocomplete(document.getElementById("addressInput"));
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById("addressInput")
+  );
 
   // Bind the map's bounds (viewport) property to the autocomplete object,
   // so that the autocomplete requests use the current map bounds for the
   // bounds option in the request.
-  autocomplete.bindTo('bounds', map);
+  autocomplete.bindTo("bounds", map);
 
   // Set the data fields to return when the user selects a place.
-  autocomplete.setFields(['address_components', 'geometry', 'icon', 'name']);
+  autocomplete.setFields(["address_components", "geometry", "icon", "name"]);
 
-  autocomplete.addListener('place_changed', function () {
+  autocomplete.addListener("place_changed", function () {
     var place = autocomplete.getPlace();
 
-    if (!place || !place.geometry || !document.getElementById("addressInput").value) {
+    if (
+      !place ||
+      !place.geometry ||
+      !document.getElementById("addressInput").value
+    ) {
       // User entered the name of a Place that was not suggested and
       // pressed the Enter key, or the Place Details request failed.
       window.alert("No details available for input");
       return;
     }
 
-    let dataFound = searchLocationsNear(place.geometry.location)
+    let dataFound = searchLocationsNear(place.geometry.location);
     if (dataFound) {
       return;
     }
@@ -87,7 +161,7 @@ function listenAutocomplete() {
       map.fitBounds(place.geometry.viewport);
     } else {
       map.setCenter(place.geometry.location);
-      map.setZoom(17);  // Why 17? Because it looks good.
+      map.setZoom(17); // Why 17? Because it looks good.
     }
   });
 }
@@ -100,42 +174,43 @@ function clearLocations() {
   }
 
   markers.length = 0;
-  locationSelect.innerHTML = "";
+  locationListWrapper.html("No data found...");
   var option = document.createElement("option");
   option.value = "none";
   option.innerHTML = "See all results:";
-  locationSelect.appendChild(option);
 }
 
-function createMarker(latlng, name, address) {
+function createMarker(latlng, name, address, city, telephone, opening_hour) {
   var html = `
-  <b>${name}</b>
-  <br/>
-  ${address}
-  `
+  <div class="marker-detail">
+  <h2 class="title">${name}</h2>
+  <span class="city">${city}</span>
+  <div class="address-wrap">
+  <span class="address">${address}</span>
+  <a href="tel:${telephone}" class="telephone">${telephone}</a>
+  <span class="time">${opening_hour}</span>
+  <a href="${getDirectionUrl(
+    address,
+    latlng.lat(),
+    latlng.lng()
+  )}" class="direction" target="_blank">Get Direction</a>
+  </div>
+  </div> 
+  `;
   var marker = new google.maps.Marker({
     map: map,
     position: latlng
   });
-  google.maps.event.addListener(marker, 'click', function () {
+  google.maps.event.addListener(marker, "click", function () {
     infoWindow.setContent(html);
     infoWindow.open(map, marker);
   });
   markers.push(marker);
 }
 
-function createOption(name, distance, num) {
-  var option = document.createElement("option");
-  option.value = num;
-  option.innerHTML = name;
-  locationSelect.appendChild(option);
-}
-
-function doNothing() { }
-
 function searchLocationsNear(center) {
   clearLocations();
-  var radius = document.getElementById('radiusSelect').value;
+  var radius = document.getElementById("radiusSelect").value;
   var data = readFromCSV(center, radius);
 
   var bounds = new google.maps.LatLngBounds();
@@ -143,26 +218,23 @@ function searchLocationsNear(center) {
   if (data.length > 0) {
     for (var i = 0; i < data.length; i++) {
       var id = data[i].id;
-      var name = data[i].name
-      var address = data[i].address
+      var name = data[i].name;
+      var address = data[i].address;
       var distance = data[i].distance;
+      var city = data[i].city;
+      var telephone = data[i].telephone;
+      var opening_hour = data[i].opening_hour;
 
       var latlng = new google.maps.LatLng(
         parseFloat(data[i].lat),
         parseFloat(data[i].lng)
       );
-
-      createOption(name, distance, i);
-      createMarker(latlng, name, address);
+      createMarker(latlng, name, address, city, telephone, opening_hour);
       bounds.extend(latlng);
     }
 
+    createLocationList(data);
     map.fitBounds(bounds);
-    locationSelect.style.visibility = "visible";
-    locationSelect.onchange = function () {
-      var markerNum = locationSelect.options[locationSelect.selectedIndex].value;
-      google.maps.event.trigger(markers[markerNum], 'click');
-    };
     return true;
   } else {
     return false;
@@ -170,27 +242,53 @@ function searchLocationsNear(center) {
 }
 
 function readFromCSV(center, radius) {
-  return csvData.map(data => {
-    let distance = null;
-    if (center && radius) {
-      distance = calculateDistance({ lat: center.lat(), lng: center.lng() }, { lat: data[7], lng: data[8] })
-    }
+  return csvData
+    .map(data => {
+      let distance = null;
+      let id = importFrom === "sheet" ? data["ID"] : data[0],
+        name = importFrom === "sheet" ? data["Name"] : data[1];
+      (city = importFrom === "sheet" ? data["City"] : data[2]),
+        (address = importFrom === "sheet" ? data["Address"] : data[3]),
+        (telephone = importFrom === "sheet" ? data["Telephone"] : data[4]),
+        (opening_hour =
+          importFrom === "sheet" ? data["Opening Hours"] : data[6]),
+        (lat = importFrom === "sheet" ? data["Latitude"] : data[7]),
+        (lng = importFrom === "sheet" ? data["Longitude"] : data[8]);
 
-    return {
-      'id': data[0],
-      'name': data[1],
-      'address': data[2],
-      'lat': data[7],
-      'lng': data[8],
-      'distance': distance,
-    }
-  }).filter(data => data.distance === null || data.distance <= radius)
+      if (center && radius) {
+        distance = calculateDistance(
+          { lat: center.lat(), lng: center.lng() },
+          { lat: lat, lng: lng }
+        );
+      }
+
+      return {
+        id: id,
+        name: name,
+        city: city,
+        address: address,
+        telephone: telephone,
+        opening_hour: opening_hour,
+        lat: lat,
+        lng: lng,
+        distance: distance
+      };
+    })
+    .filter(data => data.distance === null || data.distance <= radius);
 }
 
 function calculateDistance(point1, point2) {
-  return (3959 * Math.acos(Math.cos(radians(point1.lat)) * Math.cos(radians(point2.lat)) * Math.cos(radians(point2.lng) - radians(point1.lng)) + Math.sin(radians(point1.lat)) * Math.sin(radians(point2.lat))))
+  return (
+    3959 *
+    Math.acos(
+      Math.cos(radians(point1.lat)) *
+      Math.cos(radians(point2.lat)) *
+      Math.cos(radians(point2.lng) - radians(point1.lng)) +
+      Math.sin(radians(point1.lat)) * Math.sin(radians(point2.lat))
+    )
+  );
 }
 
 function radians(degree) {
-  return Math.sin(degree * Math.PI / 180)
+  return Math.sin((degree * Math.PI) / 180);
 }
